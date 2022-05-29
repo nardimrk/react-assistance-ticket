286500455cb8ea625d1efe0bc1b43a4a54c7ac54
nardimrk <nardimrk@gmail.com>
2022-05-29 21:32:24 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,30 @@ const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/ErrorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
-// Connect to database
+//Connect to database
 connectDB()
 
 const app = express()
 
+//accept json/body data
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// Routes
+
+
+//Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Serve Frontend
+//Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
+    //Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    // FIX: below code fixes app crashing on refresh in deployment
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    })
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
 } else {
     app.get('/', (req, res) => {
         res.status(200).json({ message: 'Welcome to the Support Desk API' })
@@ -35,4 +34,4 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))

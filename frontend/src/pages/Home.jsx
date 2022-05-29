c38822f7af4ac86195c76c234d99cd55e43418af
nardimrk <nardimrk@gmail.com>
2022-05-29 21:41:08 +0200

    make it better

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       </section>
 
       <Link to='/new-ticket' className='btn btn-reverse btn-block'>
-        <FaQuestionCircle />Crea Ticket
+        <FaQuestionCircle />Crea Nuovo
       </Link>
 
       <Link to='/tickets' className='btn btn-block'>

0cc971901eff00151a6db499b0e00d19aefd8495
nardimrk <nardimrk@gmail.com>
2022-05-29 21:35:49 +0200

    make it better

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,18 +4,18 @@ import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
 function Home() {
   return (
     <>
-    <section className="heading">
-      <h1>In che cosa hai bisogno di aiuto?</h1>
-      <p>Scegli una delle opzioni disponibili</p>
-    </section>
+      <section className="heading">
+        <h1>In che cosa hai bisogno di aiuto?</h1>
+        <p>Scegli una delle opzioni disponibili</p>
+      </section>
 
-    <Link to='/new-ticket' className='btn btn-reverse btn-block'>
-      <FaQuestionCircle />Create Ticket
-    </Link>
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle />Crea Ticket
+      </Link>
 
-    <Link to='/tickets' className='btn btn-block'>
-      <FaTicketAlt />Visualizza i miei Ticket
-    </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt />Visualizza i miei Ticket
+      </Link>
 
     </>
   )
